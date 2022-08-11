5c1b5362c9a7f65863370e503d51a6cbd5409afa
NelsonKhor <cnkhor92@gmail.com>
2022-08-11 12:36:28 +0800

    Add drawing effect with 'mouseover' event listener

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // Create Grid with Inline-block & float
-const sketchContainer = document.getElementById("sketchContainer");
-sketchContainer.style.height = "560px";
-sketchContainer.style.width = "560px";
-sketchContainer.style.border = "1px solid red"
-sketchContainer.style.display = "inline-block";
-for (i = 0; i < 16*16; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.className = "gridBox";
-    newDiv.style.width = "35px";
-    newDiv.style.height = "35px";
-    newDiv.style.float = "left";
-    newDiv.style.backgroundColor = "lightyellow"
-    newDiv.style.box = "border-box";
-    newDiv.style.display = "inline-block";
-    sketchContainer.appendChild(newDiv);
-}
+// const sketchContainer = document.getElementById("sketchContainer");
+// sketchContainer.style.height = "560px";
+// sketchContainer.style.width = "560px";
+// sketchContainer.style.border = "1px solid red"
+// sketchContainer.style.display = "inline-block";
+// for (i = 0; i < 16*16; i++) {
+//     const newDiv = document.createElement("div");
+//     newDiv.className = "gridBox";
+//     newDiv.style.width = "35px";
+//     newDiv.style.height = "35px";
+//     newDiv.style.float = "left";
+//     newDiv.style.backgroundColor = "lightyellow"
+//     newDiv.style.box = "border-box";
+//     newDiv.style.display = "inline-block";
+//     sketchContainer.appendChild(newDiv);
+// }
 
 // Create Grid with CSS grid 
 // Create drawing area/container for the boxes:
@@ -43,11 +43,16 @@ function createGrid(rows, columns) {
         newDiv.style.backgroundColor = "lightblue"
         gridContainer.appendChild(newDiv).className = "gridBox";
     }
+    
 }
 
-function drawGrid (e) {
-
-}
-
-// default grid size
+// Default grid size
 createGrid(16,16);
+
+// hover to color
+const boxes = document.querySelectorAll('.gridBox');
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = "black";
+    });
+});
